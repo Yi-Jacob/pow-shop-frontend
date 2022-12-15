d01c376cb3f265f306b751e60471337fb3489d53
Yi-Jacob <jakeyi00@gmail.com>
2022-12-14 20:32:10 -0800

    fixed bug

--- apps/ngshop-e2e/src/integration/app.spec.ts
+++ apps/ngshop-e2e/src/integration/app.spec.ts
@@ -1,6 +1,6 @@
 import { getGreeting } from '../support/app.po';
 
-describe('pow-shop', () => {
+describe('ngshop', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
@@ -8,6 +8,6 @@ describe('pow-shop', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome pow-shop');
+    getGreeting().contains('Welcome to ngshop!');
   });
 });
