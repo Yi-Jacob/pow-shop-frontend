1f02f78805ec58ef29a0bb19f1e8a2ca020b6fd1
Yi-Jacob <jakeyi00@gmail.com>
2022-12-14 20:24:59 -0800

    updated

--- apps/pow-shop/src/app/app.component.ts
+++ apps/pow-shop/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'pow-shop-frontend-root',
+  selector: 'bluebits-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'pow-shop';
+  title = 'ngshop';
 }
