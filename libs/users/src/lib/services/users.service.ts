6b3ff07f4620ce4ff8fc19c27bc1b2d3d33df9d4
Yi-Jacob <jakeyi00@gmail.com>
2022-12-19 17:30:03 -0800

    updated service

--- libs/users/src/lib/services/users.service.ts
+++ libs/users/src/lib/services/users.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../models/user';
-import { environment } from '@env/environment';
+import { environment } from 'environments/environment';
 import * as countriesLib from 'i18n-iso-countries';
 declare const require;
 
